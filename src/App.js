163acda3132654a87d0c0d0fc5fa61e4081d5df7
abanoubmesiha <bony.nagy@gmail.com>
2019-10-16 04:48:13 +0200

    app first test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import $ from 'jquery';
 import './App.css';
+import Header from './Components/Header';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Portfolio from './Components/Portfolio';
+import Testimonials from './Components/Testimonials';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import {main,portfolio,resume,testimonials} from '../public/resumeData'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      
+      resumeData:{
+        main: main,
+        portfolio: portfolio,
+        resume: resume,
+        testimonials: testimonials
+      }
+    }
+  }
+
+
+
+
+  render() {
+    console.log(main);
+    return (
+      <div className="App">
+        <Header data={main} />
+        <About data={this.state.resumeData.main} />
+        <Resume data={this.state.resumeData.resume} />
+        <Portfolio data={this.state.resumeData.portfolio} />
+        <Testimonials data={this.state.resumeData.testimonials} />
+        <Contact data={this.state.resumeData.main} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
