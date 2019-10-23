b801560b51997a8706cbc78a8869973f4c12b643
abanoubmesiha <bony.nagy@gmail.com>
2019-10-23 17:46:44 +0200

    projects columns edit

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -28,7 +28,7 @@ class Portfolio extends Component {
       <div className="row">
          <div className="twelve columns collapsed">
             <h1>Check Out Some of My Works.</h1>
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+            <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-thirds cf">
           	   {portfolio}
             </div>
          </div>
