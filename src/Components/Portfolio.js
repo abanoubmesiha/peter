38ddd164c32c585ad8e40b9f9c661a29e46467fd
abanoubmesiha <53008015+abanoubmesiha@users.noreply.github.com>
2019-12-16 13:59:38 +0200

    portofolio edits of photos and links

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -8,7 +8,7 @@ class Portfolio extends Component {
         var imageUrl = 'images/portfolio/'+ project.image;
         return <div key={project.title} className="columns portfolio-item">
            <div className="item-wrap">
-              <a href={project.url} title="">
+              <a href={project.url} target="_blank" title="">
                  <img alt="" src={imageUrl} />
                  <div className="overlay">
                     <div className="portfolio-item-meta">
