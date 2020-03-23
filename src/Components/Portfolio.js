68f278db55041c1c7f9cd69eed32f4a3f78abe74
abanoubmesiha <bony.nagy@gmail.com>
2020-03-23 16:49:21 +0200

    PortfolioCat com if 0 false

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,45 +4,53 @@ import React, { Component } from 'react';
 function PortfolioCat(props) {
    let result = props.List.map(function(project){        
       var imageUrl = 'images/portfolio/'+ project.image;
-      return <div key={project.title} className="columns portfolio-item">
+            return( 
+               
+            <div key={project.title} className="columns portfolio-item">
          <div className="item-wrap">
             <a href={project.url} target="_blank" title="">
                <img alt="" src={imageUrl} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
-                 <h5>{project.title}</h5>
-                     <p>{project.category}</p>
+                 <h5>{project.title}</h5><p>{project.category}</p>
               </div>
                </div>
             </a>
          </div>
-    
      </div>
+     )
     });
- return result;
+    
+ return ( result.length == 0)? false :(
+   <div id={props.Title +'-wrapper'} className="bgrid-thirds s-bgrid-thirds cf">
+   <h4 style={{color:'#8bc34a'}}>{props.Title}</h4>
+   {result}
+   </div>
+ );
    }
 
 class Portfolio extends Component {
   render() {
     if(this.props.data){
        var portfolioAngular = this.props.data.projects.filter(p=>{return p.category.includes('Angular')});
-
+       var portfolioReact = this.props.data.projects.filter(p=>{return p.category.includes('React')});
+       var portfolioJS = this.props.data.projects.filter(p=>{return p.category.includes('Javascript')});
+       var portfolioNode = this.props.data.projects.filter(p=>{return p.category.includes('Node')});
+       var AllCat = Array.prototype.concat(portfolioAngular, portfolioReact, portfolioJS,portfolioNode);
+       var portfolioOther = this.props.data.projects.filter(p => !AllCat.includes(p));
     }
     return (
       <section id="portfolio">
       <div className="row">
          <div className="twelve columns collapsed text-center">
             <h1>Check Out Some of My Works.</h1>
-            <h4 style={{color:'#8bc34a'}}>React</h4>
-            <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-thirds cf">
-            <PortfolioCat List={portfolioReact} />
-            </div>
-            <h4 style={{color:'#8bc34a'}}>Angular</h4>
-            <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-thirds cf">
-            <PortfolioCat List={portfolioAngular} />
+            <PortfolioCat Title={'React'} List={portfolioReact} />
+            <PortfolioCat Title={'Angular'} List={portfolioAngular} />
+            <PortfolioCat Title={'Javascript'} List={portfolioJS} />
+            <PortfolioCat Title={'Node.js'} List={portfolioNode} />
+            <PortfolioCat Title={'Other'} List={portfolioOther} />
             </div>
          </div>
-      </div>
    </section>
     );
   }
