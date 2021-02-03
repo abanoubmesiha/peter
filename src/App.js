72cc5df69b52b8552f95c75440e2166528b79bf4
Abanoub Ayoub <abanoub@terkwaz.com>
2021-02-03 23:36:24 +0200

    Fix build.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Components/Header';
 import About from './Components/About';
-import Resume from './Components/Resume';
 import Portfolio from './Components/Portfolio';
 import Footer from './Components/Footer';
 import {main,portfolio,resume,testimonials} from './resumeData.json'
