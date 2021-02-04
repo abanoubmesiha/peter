1d6c71221f1094b6ae315fdedca545a52b4bf369
Abanoub Ayoub <abanoub@terkwaz.com>
2021-02-04 17:50:38 +0200

    * Update About.
    * Update images size.
    
    * Update `Personal` images url.

--- src/Components/About.js
+++ src/Components/About.js
@@ -23,24 +23,43 @@ class About extends Component {
          </div>
          <div className="nine columns main-col">
             <h2>Current Qualifications</h2>
-            <p>{bio}
-            </p>
+            <ul>
+               {bio.map((point, i)=>(<li key={i}>{point}</li>))}
+               <li>
+               Worked in online marketing before joining Novartis and it helped me to own a group of social Facebook pages with a total followers reach more than 6 million followers. 
+                  <ol>
+                     <li><a href="https://www.facebook.com/Connect4.Online" rel="noreferrer">https://www.facebook.com/Connect4.Online</a></li>
+                     <li><a href="https://www.facebook.com/Sharek.Br2yak" rel="noreferrer">https://www.facebook.com/Sharek.Br2yak</a></li>
+                     <li><a href="https://www.facebook.com/A3gbny.Like" rel="noreferrer">https://www.facebook.com/A3gbny.Like</a></li>
+                     <li><a href="https://www.facebook.com/Ed7ak.Nokat.Page" rel="noreferrer">https://www.facebook.com/Ed7ak.Nokat.Page</a></li>
+                     <li><a href="https://www.facebook.com/Ekteb.Nokta" rel="noreferrer">https://www.facebook.com/Ekteb.Nokta</a></li>
+                     <li><a href="https://www.facebook.com/7ob.Romancya" rel="noreferrer">https://www.facebook.com/7ob.Romancya</a></li>
+                     <li><a href="https://www.facebook.com/M3louma.Themk" rel="noreferrer">https://www.facebook.com/M3louma.Themk</a></li>
+                     <li><a href="https://www.facebook.com/Love.7ob.Page" rel="noreferrer">https://www.facebook.com/Love.7ob.Page</a></li>
+                     <li><a href="https://www.facebook.com/Carcter" rel="noreferrer">https://www.facebook.com/Carcter</a></li>
+                     <li><a href="https://www.facebook.com/A7laaa.kalam" rel="noreferrer">https://www.facebook.com/A7laaa.kalam</a></li>
+                     <li><a href="https://www.facebook.com/2020Egypt" rel="noreferrer">https://www.facebook.com/2020Egypt</a></li>
+                  </ol>
+               </li>
+            </ul>
+            <h2>My Objective</h2>
+            <p>Following a developmental plan to empower my technical qualifications with the important academic knowledge of Digital Marketing Principles and Requirements to help me join Novartis Digital Marketing Team.</p>
             <div className="row">
                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
+                  <h2 style={{marginBottom: '5px'}}>Contact Details</h2>
                   <p className="address">
-						   <span>{name}</span><br />
+						   {/* <span>{name}</span><br /> */}
 						   {state} 
                      <br />
 						   <span>{phone}</span><br />
                      <span>{email}</span>
 					   </p>
                </div>
-               <div className="columns download">
+               {/* <div className="columns download">
                   <p>
                      <a href={resumeDownload} target="_blank" rel="noopener noreferrer" className="button"><i className="fa fa-download"></i>Download Resume</a>
                   </p>
-               </div>
+               </div> */}
             </div>
          </div>
       </div>
