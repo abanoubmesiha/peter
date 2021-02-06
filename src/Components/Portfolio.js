cf758a4d597161b5a885f15daf3f12159de2aa87
abanoubmesiha <53008015+abanoubmesiha@users.noreply.github.com>
2021-02-06 12:52:46 -0500

    Personal and work sections

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -12,7 +12,7 @@ function PortfolioCat(props) {
                <img alt="" src={imageUrl} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
-                 <h5>{project.title}</h5><p>{project.category}</p>
+                 <h5>{project.title}</h5>
               </div>
                </div>
             </a>
@@ -32,21 +32,23 @@ function PortfolioCat(props) {
 class Portfolio extends Component {
   render() {
     if(this.props.data){
+       var portfolioWork = this.props.data.projects.filter(p=>{return p.category.includes('Work')});
        var portfolioPersonal = this.props.data.projects.filter(p=>{return p.category.includes('Personal')});
        var portfolioNovartis = this.props.data.projects.filter(p=>{return p.category.includes('Novartis')});
       //  var AllCat = Array.prototype.concat(portfolioPersonal, portfolioNovartis);
     }
     return (
       <section id="portfolio">
-      <div className="row">
-         <div className="twelve columns collapsed text-center">
-            <h1 style={{marginLeft: '2rem', marginRight: '2rem'}}>Check below some of my ideas i have executed in graghic designs and videos either in my work for Novartis or in my personal life.</h1>
-    
-            <PortfolioCat Title={'Videos'} List={portfolioNovartis} />
-            <PortfolioCat Title={'Photo Designs'} List={portfolioPersonal} />
-            </div>
-         </div>
-   </section>
+          <div className="row">
+             <div className="twelve columns collapsed text-center">
+                <h1 style={{marginLeft: '2rem', marginRight: '2rem'}}>Check below some of my ideas i have executed in graphic designs and videos either in my work for Novartis or in my personal life.</h1>
+
+                <PortfolioCat Title={'Videos'} List={portfolioNovartis} />
+                <PortfolioCat Title={'Work Photo Designs'} List={portfolioWork} />
+                <PortfolioCat Title={'Personal Photo Designs'} List={portfolioPersonal} />
+                </div>
+             </div>
+       </section>
     );
   }
 }
